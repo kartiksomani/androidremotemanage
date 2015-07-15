5331c330dcb6049b2aafed931b15b19586d49e14
Kartik Somani <kartiksomani@ymail.com>
2015-07-15 14:51:10 +0530

    Fetching contact name if available, and sending it to server

--- App/src/androidremotemanage/app/CallReceiver.java
+++ App/src/androidremotemanage/app/CallReceiver.java
@@ -1,9 +1,13 @@
 package androidremotemanage.app;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.telephony.TelephonyManager;
+import android.provider.ContactsContract;
+import android.net.Uri;
+import android.database.Cursor;
 
 public class CallReceiver extends BroadcastReceiver {
     Notification notification;
@@ -13,6 +17,30 @@ public class CallReceiver extends BroadcastReceiver {
         notification = new Notification(ipAddress,Integer.parseInt(port));
     }
 
+    public String getCallerName(Context ctx,String phoneNumber) {
+        String callerName;
+        
+        callerName = "";
+        String [] projection = new String[] {
+            ContactsContract.PhoneLookup.DISPLAY_NAME,
+            ContactsContract.PhoneLookup.NUMBER,
+            ContactsContract.PhoneLookup.HAS_PHONE_NUMBER
+        };
+        Uri contactUri = Uri.withAppendedPath(
+                                              ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+                                              Uri.encode(phoneNumber));
+        Cursor cursor = ctx.getContentResolver().
+            query(contactUri,projection,null,null,null);
+        if(cursor.moveToFirst()) {
+            callerName = cursor.getString(cursor
+                                          .getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+        }
+        cursor.close();
+        if(callerName.equals("")) {
+            callerName = phoneNumber;
+        }
+        return callerName;
+    }
     @Override
     public void onReceive(Context ctx,Intent intent) {
         String caller;
@@ -26,7 +54,7 @@ public class CallReceiver extends BroadcastReceiver {
             if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
                 incomingNumber =
                     intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                caller = incomingNumber;
+                caller = getCallerName(ctx,incomingNumber);
                 notification.sendNotification(NotificationType.Call,caller,message);
             }
         }
